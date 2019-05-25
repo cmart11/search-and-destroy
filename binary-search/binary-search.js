66cc436bc1211785b0fe4e4c77f9de81d7467580
Cristian Martinez <2cristianmartinez@gmail.com>
2019-05-25 11:04:35 -0400

    bts done

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,8 +2,29 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	if (array.length === 1 && array[0] === target) {
+		return true
+	}
+	else if (array.length === 1 && array[0] !== target || !array.length) {
+		return false;
+	}
+
+	if (array[middle] === target) {
+		return true
+	}
+	else if (array[middle] < target) {
+		return binarySearch(right, target);
+	}
+	else if (array[middle] >= target) {
+		return binarySearch(left, target)
+	} else {
+		return false
+	}
 };
+
+// binarySearch([1,3,4,5,10,15,20,22,23,98,1000], 100)
+
+// binarySearch([1], 100)
 
 /*
 	EXTRA CREDIT:
